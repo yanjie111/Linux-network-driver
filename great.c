52b6e3f5bc1e563678f2cd87103e4d328dd633e9
yanjie111 <yanjie111@yahoo.com>
2015-01-21 22:38:33 -0500

    Add copyright info and add chapter 2 of tutorial.

--- great.c
+++ great.c
@@ -4,8 +4,18 @@
  */
 #include <linux/module.h> // definitions of symbols and functions needed by loadable modules	
 #include <linux/init.h>	// specify initialization and cleanup functions
+/* insert code area 1 here for chapter 3.2 */
 
-// All the other codes in the following steps will be added here 
+#define GREAT_DRV_VERSION "1.0.0.0"
+char great_driver_name[] = "great";
+char great_driver_version[] = GREAT_DRV_VERSION;
+
+MODULE_AUTHOR("Jie Yan  <yanjie111@gmail.com>");
+MODULE_DESCRIPTION("   - for Linux version 3.5.0");
+MODULE_DESCRIPTION("sample of <Linux Ethernet device driver demystified>");
+MODULE_LICENSE("GPL");
+MODULE_VERSION(GREAT_DRV_VERSION);
+/* insert code area 2 here for chapter 3.2 */
 
 /*
  * great_init_module - is the first routine called when the driver is loaded.
@@ -17,6 +27,7 @@
 static int __init great_init_module(void)
 {	// There is no printf in kernel. 
 	printk(KERN_INFO "Init great network driver.\n");
+	/* insert code area 3 here for chapter 3.2 */
 	return 0;
 }
 /*
@@ -28,9 +39,11 @@ static int __init great_init_module(void)
 static void __exit great_exit_module(void)
 {
 	printk(KERN_INFO "Cleanup great network driver.\n");
+	/* insert code area 4 here for chapter 3.2 */
 }
 // module_init macro adds a special section to the module's object code
 // stating where the module's initialization function is to be found
 module_init(great_init_module);
+
 // module_exit macro enables kernel to find the cleanup function
 module_exit(great_exit_module);
